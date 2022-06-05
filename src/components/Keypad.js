1e44dd72f54e682ce1653be6996de227b199c3c5
Francisco Mesar <fmesar87@gmail.com>
2022-06-05 12:13:47 -0400

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+//  <Keypad>
+            
+        
+//
+//</Keypad>
+import React from "react";
+
+function Keypad() {
+  
+   
+  
+    return (
+        <Keypad>
+            <input onChange={e => console.log(`Entering password ${e.target.value}`)}type="text" name="username" placeholder="Enter username..." />
+        </Keypad>
+      )
+}
+
+export default Keypad;
